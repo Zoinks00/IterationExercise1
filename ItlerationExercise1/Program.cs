d7f776a4e62be6f3100423246e1801a4d4e9bba1
Forrest Rabideau <forrest.rabideau@gmail.com>
2021-11-08 11:43:14 -0800

    1st commit

---
+++ ItlerationExercise1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ItlerationExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // i starts at 0 and adds 1 to the value while less than 15
+            for (int i = 0; i < 15; i++ )
+            { 
+                // print results starting at 0 up to 14
+                // total of 15 printouts to cmd
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
